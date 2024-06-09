3a9439923a5f458db4543d07b70f400748b33dfd
Enoch Roots <enoch.roots@gmail.com>
2024-06-09 16:36:09 -0500

    hide cover

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import BackgroundVideo from "./components/BGVideo";
 import FullPhoto from "./components/FullPhoto";
 import FullPhoto2 from "./components/FullPhoto2";
 import BlocksContainer from "./components/BlocksContainer";
-import { CoverVid } from "./components/CoverVid";
+// import { CoverVid } from "./components/CoverVid";
 import { useState } from "react";
 import { Footer } from "./components/Footer";
 
 function App() {
-  const [hasTouched, setHasTouched] = useState(false);
+  // const [hasTouched, setHasTouched] = useState(false);
+  const hasTouched = true;
   const [isVideoVisible, setIsVideoVisible] = useState(true);
   const handleVideoEnd = () => {
     setIsVideoVisible(false);
   };
-  const handleTouch = () => {
-    setHasTouched(true);
-  };
+  // const handleTouch = () => {
+  //   setHasTouched(true);
+  // };
 
   const lockoutClass = isVideoVisible ? "lockout" : "";
 
